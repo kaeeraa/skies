d0f288a877474236020a9a70fa577bd60ee54e77
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-10-26 22:58:23 +0300

    feat: implement secret spec, cachix, git hooks
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- devenv.nix
+++ devenv.nix
@@ -1,13 +1,38 @@
 {
   pkgs,
-  lib,
   config,
-  inputs,
   ...
 }: {
   # https://devenv.sh/packages/
-  packages = [pkgs.git];
+  packages = [
+    pkgs.git
+    pkgs.ninja
+    pkgs.boost
+    pkgs.secretspec
+  ];
 
   # https://devenv.sh/languages/
-  languages.cplusplus.enable = true;
+  languages = {
+    cplusplus.enable = true;
+    nix.enable = true;
+  };
+
+  cachix = {
+    enable = true;
+
+    pull = ["kaeeraa"];
+    push = "kaeeraa";
+  };
+
+  git-hooks.hooks = {
+    shellcheck.enable = true;
+    mdsh.enable = true;
+    alejandra.enable = true;
+    cmake-format.enable = true;
+    deadnix.enable = true;
+    markdownlint.enable = true;
+    statix.enable = true;
+  };
+
+  env.CACHIX_AUTH_TOKEN = config.secretspec.secrets.CACHIX_AUTH_TOKEN or "";
 }
